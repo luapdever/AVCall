c296bf1be1adebc928cb3f1b0aa4f966dccfb03f
Luap Dever <luapdever511@gmail.com>
2023-01-21 19:39:13 +0100

    try fix streaming problem

--- src/helpers/permissions.js
+++ src/helpers/permissions.js
@@ -2,8 +2,8 @@ export function getLocalStream() {
   navigator.mediaDevices
     .getUserMedia({ video: true, audio: true })
     .then((stream) => {
-      // stream.getTracks()
-      // .forEach(track => track.stop());
+      stream.getTracks()
+      .forEach(track => track.stop());
 			window.localStream = stream;
       // window.localAudio.srcObject = stream;
       // window.localAudio.autoplay = true;
